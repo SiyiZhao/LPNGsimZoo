a646aef311a357531b076a100045a38f697309e7
Siyi Zhao <52352980+SiyiZhao@users.noreply.github.com>
2025-05-17 13:22:34 +0800

    test pkdgrav3 on xianhc

--- codes/compare_nbody/run_bg.sh
+++ codes/compare_nbody/run_bg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# @r88
 
 PREFIX="logs/$1"
 
--- codes/compare_nbody/run_convertic.sh
+++ codes/compare_nbody/run_convertic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # @r88
 
-python3 convert_IC.py /home/czhao/sims/IC_Quijote/raw/ics /home/czhao/sims/IC_Quijote/ics_for_pkdgrav3_new.hdf5
+python3 convert_IC.py /home/czhao/sims/IC_Quijote/raw/ics /home/czhao/sims/IC_Quijote/ics_for_pkdgrav3_correct.hdf5
